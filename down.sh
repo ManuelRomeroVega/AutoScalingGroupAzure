7bf67ad64f0af8401923597ed20f9a4d522e9b5c
Manuel Romero Vega <162843658+ManuelRomeroVega@users.noreply.github.com>
2025-12-28 11:28:58 +0100

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,6 +1,6 @@
-$Thumbprint = "9E845CAE4987AA2A04FCF10F80416E966437112B"
-$TenantId = "d2cc63fc-6c18-4213-bf42-f2001e89e788"
-$ApplicationId = "44fc1beb-6709-4f7b-80e4-07e22e123ca7"
+$Thumbprint = ""
+$TenantId = ""
+$ApplicationId = ""
 
 Connect-AzAccount -CertificateThumbprint $Thumbprint -ApplicationId $ApplicationId -Tenant $TenantId -ServicePrincipal
 
@@ -13,4 +13,5 @@ $vm = Get-AzVM -ResourceGroupName $resourceGroup -Name $vmName
 # Change the VM size
 $vm.HardwareProfile.VmSize = $size
 # Update the VM
+
 Update-AzVM -ResourceGroupName $resourceGroup -VM $vm
